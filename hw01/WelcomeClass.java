cbe7ee71b713c439433160ae5ef094d9f7c6aa5d
map218 <map218@lehigh.edu>
2005-12-13 05:06:07 +0000

    testing

--- hw01/WelcomeClass.java
+++ hw01/WelcomeClass.java
@@ -1,30 +1,22 @@
-///////////////////////////////////////////////////////////////////////////////
 //Matt Palmieri
-//Homework 1
-//Welcome Class Java Program
-//
-//   first compile the program
-//      javac WelcomeClass.java
-//   run the program
-//      java WelcomeClass//
-
-//   define a class
+//CSE 002
+//HW01
+//1/24/15
+// define class
 public class WelcomeClass{
-    
-//   add main method
-   public static void main(String[] args)  {
-       
-//   print out the welcome message with the box around it, lehigh id signature and an autobiographic statement
-     System.out.println("  -----------  ");
-     System.out.println("  | WELCOME |  ");
-     System.out.println("  -----------  ");
-     System.out.println("  ^  ^  ^  ^  ^  ^  ");
-     System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\ ");
-     System.out.println("<-M--A--P--2--1--8->");
-     System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ / ");
-     System.out.println("  v  v  v  v  v  v  ");
-     System.out.println("I am a first year eningeering student at Lehigh University");
-     
-     //test/testtest
-   }
+    //add main method
+    public static void main(String[] args){
+    // make sure two\\ are added to avoid error
+        
+        //print my statement
+        System.out.println("                ----------- ");
+        System.out.println("                | WELCOME | ");
+        System.out.println("                ----------- ");
+        System.out.println("              ^  ^  ^  ^  ^  ^");
+        System.out.println("             / \\/ \\/ \\/ \\/ \\/ \\ ");
+        System.out.println("            |-M--A--P--2--1--8-|");
+        System.out.println("             \\ /\\ /\\ /\\ /\\ /\\ / ");
+        System.out.println("              v  v  v  v  v  v ");
+        System.out.println("Im Matt and I'm a student at Lehigh");
+    }
 }
